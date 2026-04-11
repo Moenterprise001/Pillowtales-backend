8cb02f4b2f9d43beac2faaf843231d087aa01498
PillowTales Dev <moenterprise001@gmail.com>
2026-04-11 20:26:59 +0200

    fix routes init syntax

--- app/api/routes/__init__.py
+++ app/api/routes/__init__.py
@@ -1,18 +1,14 @@
 from fastapi import APIRouter
 
-from app.api.routes import auth
-from app.api.routes import catalog
-from app.api.routes import narration
-from app.api.routes import subscription
-from app.api.routes import user
-from app.api.routes import parent_voice
+from app.api.routes import auth, catalog, narration, stories, subscription, system, user, parent_voice
 
 api_router = APIRouter()
 
 api_router.include_router(auth.router)
 api_router.include_router(catalog.router)
 api_router.include_router(narration.router)
+api_router.include_router(stories.router)
 api_router.include_router(subscription.router)
+api_router.include_router(system.router)
 api_router.include_router(user.router)
-api_router.include_router(parent_voice.router)from app.api.routes import parent_voice
 api_router.include_router(parent_voice.router)
